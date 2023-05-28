bbc2d2c2f6e7e31bf0064d275ac5dd2bbff9d533
Renan <114445014+renanolivex@users.noreply.github.com>
2023-05-28 12:07:10 -0300

    Exercicios resolvidos

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+
+export type Tusers = {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    createdAt: string,
+}
+
+export type Tproducts ={
+    id:string,
+    name: string,
+    price: number,
+    description: string,
+    imageUrl: string,
+}
