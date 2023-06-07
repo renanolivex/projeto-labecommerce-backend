a4c388bfc48ee2c02440f4b07c47add1491c2421
Renan <114445014+renanolivex@users.noreply.github.com>
2023-06-06 21:21:56 -0300

    Exercicios de apis-express feitos

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import {  createProduct, createUser, getAllProducts, getAllUsers,   products, searchProductsByName, users } from "./database"
+import express, {Request, Response} from 'express'
+import cors from 'cors'
+import { TProducts, TUsers } from "./types"
+
 
 console.log("Aplicativo foi iniciado") 
 /* 
@@ -17,3 +21,64 @@ console.log(searchProductsByName(products, "Mouse gamer"))
 
 console.log(getAllUsers()) 
 console.log(getAllProducts())
+
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+})
+
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
+})
+//Pegar usuarios
+app.get("/users",(req:Request, res:Response)=>{
+    res.status(200).send(users)
+})
+
+app.get("/products",(req:Request, res:Response)=>{
+    
+    const name=req.query.name as string
+    let response
+    if(name){
+
+        response = searchProductsByName(products, name)
+   }else {response = products}
+   res.status(200).send(response)
+})
+
+app.post("/users", (req:Request, res:Response)=>{
+    const {id, name, email, password, createdAt}= req.body
+    
+    const newUser:TUsers = {
+        id: id,
+        name: name,
+        email: email,
+        password: password,
+        createdAt: createdAt
+        
+    }
+
+    users.push(newUser);
+    res.status(201).send("Usuario registrado com sucesso")
+})
+
+app.post("/products", (req:Request, res:Response)=>{
+    const {id, name, price, description, imageUrl}= req.body
+    
+    const newProduct:TProducts = {
+        id: id,
+        name: name,
+        price: price,
+        description: description,
+        imageUrl: imageUrl,              
+    }
+
+    products.push(newProduct);
+    res.status(201).send("Produto registrado com sucesso")
+})
+
