c8fa001720ead2ee53138faa95a2250639fb9a0a
Renan <114445014+renanolivex@users.noreply.github.com>
2023-06-22 20:46:52 -0300

    Exercicios de aprofundamento-sql

--- src/labecommerce.sql
+++ src/labecommerce.sql
@@ -31,5 +31,49 @@ VALUES ("prod001","Mouse gamer", 250,"Melhor mouse do mercado!", "https://picsum
 ("prod005","Pen Drive", 18.55,"8GB, PD587", "https://http2.mlstatic.com/pen-drive-sandisk-8-gb-cruzer-blade-100-original-o-melhor-D_NQ_NP_18442-MLB20156048733_092014-F.jpg");
 
 
+--Retornar todas as pessoas cadastradas
+
+SELECT * FROM users;
+
+
+--Retornar todos os produtos cadastrados
+
+SELECT * FROM products;
+
+--Retorna apenas produtos com "gamer" no nome
+
+SELECT * FROM products WHERE name LIKE "%gamer";
+
+--Add novo usuario
+
+INSERT INTO users (id, name, email, password, created_at)
+VALUES ("u004", "Fulano4", "fulano4@email.com", "fulano44442",  "22/06/2022" );
+
+--Add novo produto
+
+INSERT INTO products (id, name, price, description, image_url)
+VALUES ("prod006","Óculos gamer", 199.90,"Óculos para passar um bom tempo jogando", "https://images.kabum.com.br/produtos/fotos/373823/oculos-para-jogos-gunnar-vertex-armacao-quadrada-nix-lentes-gunnar-focus-cor-ambar-max-lens-ver-00112_1668770092_gg.jpg");
+
+--Excluir User
+DELETE FROM users WHERE id = 'u004';
+
+--Excluir Product
+
+DELETE FROM products WHERE id = 'prod006';
+
+--Editar produto por ID
+
+UPDATE products 
+SET name = "novo Nome"
+WHERE id = "prod006";
+UPDATE products 
+SET price = 50
+WHERE id = "prod006";
+UPDATE products 
+SET description = "nova descrição"
+WHERE id = "prod006";
+UPDATE products 
+SET image_url = "nova image_url"
+WHERE id = "prod006"
 
 
